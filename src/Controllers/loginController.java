6968003ec61fb947050f69cd136a686f3d0c5484
maxmorales <maxymorales52@gmail.com>
2020-11-26 16:21:30 -0500

    Initial commit

---
+++ src/Controllers/loginController.java
@@ -0,0 +1,20 @@
+package Controllers;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class loginController {
+
+    @FXML
+    private TextField passwordText;
+
+    @FXML
+    private TextField userIDText;
+
+    @FXML
+    void onActionLogin(ActionEvent event) {
+
+    }
+}
---
+++ src/Utils/DBConnection.java
@@ -0,0 +1,57 @@
+package Utils;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static java.lang.Class.forName;
+
+/**
+ * This is the database driver connection class
+ */
+public class DBConnection {
+
+    // JDBC URL Parts
+    private static final String protocol = "jdbc";
+    private static final String subProtocol = ":mysql:";
+    private static final String subName = "//wgudb.ucertify.com/WJ07gew";
+
+    // JDBC URL
+    private static final String jdbcURL = protocol + subProtocol + subName;
+
+    // JDBC driver
+    private static final String driver = "com.mysql.cj.jdbc.Driver";
+
+    // Opening up the Connection
+    private static Connection con = null;
+    private static String username = "U07gew";
+    private static String password = "53689021687";
+
+    public static Connection startConnection() {
+        try {
+            Class.forName(driver);
+            con = (Connection)DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connection Success");
+        }
+        catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage() + "f");
+        }
+        catch (SQLException e) {
+            System.out.println(e.getMessage() + "e");
+        }
+        return con;
+    }
+
+    // Closing the connection
+    public static void closeConnection() throws SQLException {
+        try {
+            con.close();
+            System.out.println("Connection Closed");
+        } catch(SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+
+}
